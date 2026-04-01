eed0b5a1c39e0ec8363068c081ce861cb27590aa
sumi3desu <developer@bouzer.jp>
2026-04-02 02:53:00 +0900

    釣果の曖昧表示なしのしての場合は実際の釣果があった釣り場を明示する動作にした

--- lib/constants.dart
+++ lib/constants.dart
@@ -6,3 +6,4 @@ const int kPostPageSize = 20; // 投稿一覧の1ページ件数
 final String _tosUrl = 'asset://assets/policies/terms_of_use.html';
 final String _privacyUrl = 'asset://assets/policies/privacy_policy.html';
  
+const bool ambiguous_point = false;
